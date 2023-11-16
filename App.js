69adb4691079c4448c28d0f963df8b3335f27cd1
YesicaChui <yesicachuic@gmail.com>
2023-11-15 20:05:57 -0500

    agregando un texto adicional

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text>Hola, Coder!</Text>
       <Text>clase2 Yesica</Text>
+      
       <StatusBar style="auto" />
     </View>
   );
